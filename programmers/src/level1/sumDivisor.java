bdfea0ea11411499dc11dc4520e280c4447074b2
Heejin Im <85548480+dlagmlwls39@users.noreply.github.com>
2023-02-08 23:22:23 +0900

    약수의 합
    약수의 합

--- programmers/src/level1/sumDivisor.java
+++ programmers/src/level1/sumDivisor.java
@@ -11,15 +11,15 @@ public class sumDivisor {
 	}
 	
 	public static int solution(int n) {
-        int answer = 0;
+        	int answer = 0;
         
-        for (int i = 1; i <= n ; i++) { 
-            if (n % i == 0) { 
-                answer += i;
-            }
-        }
+        	for (int i = 1; i <= n ; i++) { 
+           		if (n % i == 0) { 
+              			answer += i;
+           		}
+        	}
         
-        return answer;
-    }
+        	return answer;
+  	}
 	
 }

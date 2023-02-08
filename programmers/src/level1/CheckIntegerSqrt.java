3a1b58945b872e16d00f5012585edd8a81245eff
Heejin Im <85548480+dlagmlwls39@users.noreply.github.com>
2023-02-09 01:20:28 +0900

    정수 제곱근 판별

--- programmers/src/level1/CheckIntegerSqrt.java
+++ programmers/src/level1/CheckIntegerSqrt.java
@@ -11,25 +11,25 @@ public class CheckIntegerSqrt {
 	}
 	
 	public static long solution(long n) {
-        long answer = 0;
+        	long answer = 0;
 
-        // 방법 1
-        /*
-        for (long i = 1; i <= n; i++) {
-            if (i * i == n) {
-                answer = (i + 1) * (i + 1);
-                break;
-            } else { 
-                answer = -1;
-            }
-        }
-        */
+        	// 방법 1
+        	/*
+        	for (long i = 1; i <= n; i++) {
+            		if (i * i == n) {
+                		answer = (i + 1) * (i + 1);
+                		break;
+            		} else { 
+               		answer = -1;
+            		}
+        	}
+        	*/
         
-        // 방법 2
-        double x = Math.sqrt(n);
-        answer = Math.pow((int)x, 2) == n ? (long)Math.pow(x + 1, 2) : -1;
+        	// 방법 2
+        	double x = Math.sqrt(n);
+        	answer = Math.pow((int)x, 2) == n ? (long)Math.pow(x + 1, 2) : -1;
         
-        return answer;
-    }
+        	return answer;
+    	}
 	
 }

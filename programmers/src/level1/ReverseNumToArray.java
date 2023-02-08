f552beec9f556b25e0079ae083fd2727a4d7b9b0
Heejin Im <85548480+dlagmlwls39@users.noreply.github.com>
2023-02-09 01:18:06 +0900

    자연수 뒤집어 배열로 만들기

--- programmers/src/level1/ReverseNumToArray.java
+++ programmers/src/level1/ReverseNumToArray.java
@@ -11,16 +11,16 @@ public class ReverseNumToArray {
 	}
 	
 	public static int[] solution(long n) {
-        String str = String.valueOf(n);
-        int[] answer = new int[str.length()];
+        	String str = String.valueOf(n);
+        	int[] answer = new int[str.length()];
         
-        int i = 0;
-        while (n > 0) {
-            answer[i++] = (int)(n % 10);
-            n /= 10;
-        }
+        	int i = 0;
+        	while (n > 0) {
+            		answer[i++] = (int)(n % 10);
+            		n /= 10;
+       		}
         
-        return answer;
-    }
+        	return answer;
+    	}
 	
 }

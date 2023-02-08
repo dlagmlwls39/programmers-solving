73c22ba0472a260907aa9218ed3245f856a2bc5d
dlagm <dlagm@DESKTOP-HK82DOS>
2023-02-09 01:13:24 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ programmers/src/level1/IntervalOfX.java
@@ -0,0 +1,25 @@
+// x만큼 간격이 있는 n개의 숫자(https://school.programmers.co.kr/learn/courses/30/lessons/12954)
+
+package level1;
+
+import java.util.*;
+
+public class IntervalOfX {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		long x = scanner.nextInt();
+		int n = scanner.nextInt();
+		System.out.println(Arrays.toString(solution(x, n)));
+	}
+	
+	public static long[] solution(long x, int n) {
+        long[] answer = new long[n];
+        for(int i = 0; i < n; i++) {
+            answer[i] = x * (i + 1);
+        }
+        
+        return answer;
+    }
+	
+}

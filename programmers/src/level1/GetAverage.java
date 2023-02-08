323a7cf954c2cb1b7f82086e163d4d7b5d40a23a
dlagm <dlagm@DESKTOP-HK82DOS>
2023-02-08 23:57:47 +0900

    평균 구하기

---
+++ programmers/src/level1/GetAverage.java
@@ -0,0 +1,29 @@
+package level1;
+
+public class GetAverage {
+	
+	public static void main(String[] args) {
+		int[] n = { 1,2,3,4 };
+       	System.out.println(solution(n));
+	}
+	
+	public static double solution(int[] arr) {
+		if(arr == null || arr.length == 0){
+        	return 0;
+      	}
+		
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        
+        /*
+        for(int n : arr) {
+            sum += n;
+        }
+        */
+        
+        return (double) sum / arr.length;
+    }
+	
+}

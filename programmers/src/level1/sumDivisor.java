0a3902819716d8c9777662b94da49eaa724129b3
Heejin Im <85548480+dlagmlwls39@users.noreply.github.com>
2023-02-08 23:21:16 +0900

    약수의 합
    약수의 합

--- programmers/src/level1/sumDivisor.java
+++ programmers/src/level1/sumDivisor.java
@@ -7,7 +7,7 @@ public class sumDivisor {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
-        System.out.println(solution(n));
+       		System.out.println(solution(n));
 	}
 	
 	public static int solution(int n) {

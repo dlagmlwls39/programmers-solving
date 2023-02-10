4c0e647b60d8ba192a04053dd8539ab947441452
dlagm <dlagm@DESKTOP-HK82DOS>
2023-02-11 01:48:55 +0900

    없는 숫자 더하기

---
+++ programmers/src/level1/MissingNumber.java
@@ -0,0 +1,23 @@
+// 없는 숫자 더하기(https://school.programmers.co.kr/learn/courses/30/lessons/86051)
+
+package level1;
+
+import java.util.*;
+
+public class MissingNumber {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int[] numbers = {1, 2, 3, 4, 6, 7, 8, 0 };
+		System.out.println(solution(numbers));
+	}
+	
+	public static int solution(int[] numbers) {
+		int sum = 45;
+		for (int i : numbers) {
+			sum -= i;
+		} 
+		return sum;
+	}
+	
+}

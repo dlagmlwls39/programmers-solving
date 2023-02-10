2c6a6cb2e37f042fb4924d9f74b354e6f76d775d
dlagm <dlagm@DESKTOP-HK82DOS>
2023-02-11 01:14:19 +0900

    핸드폰 번호 가리기

---
+++ programmers/src/level1/HidePhoneNumber.java
@@ -0,0 +1,25 @@
+// 핸드폰 번호 가리기(https://school.programmers.co.kr/learn/courses/30/lessons/12948)
+
+package level1;
+
+import java.util.*;
+
+public class HidePhoneNumber {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String phone_number = scanner.next();
+		System.out.println(solution(phone_number));
+	}
+	
+	public static String solution(String phone_number) {
+        String answer = "";
+        int index = phone_number.length() - 4;
+        for (int i = 0; i < index; i++) {
+            answer += "*";
+        }
+        answer += phone_number.substring(index);
+        return answer;
+    }
+	
+}

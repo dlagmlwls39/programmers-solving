0dd8d76552936fe8d41825d3180935b27389a4ea
dlagm <dlagm@DESKTOP-HK82DOS>
2023-02-11 01:43:34 +0900

    음양 더하기

---
+++ programmers/src/level1/SumNegPos.java
@@ -0,0 +1,24 @@
+// 음양 더하기(https://school.programmers.co.kr/learn/courses/30/lessons/76501)
+
+package level1;
+
+import java.util.*;
+
+public class SumNegPos {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int[] absolutes = { 4, 7, 12 };
+		boolean[] signs = { true, false, true };
+		System.out.println(solution(absolutes, signs));
+	}
+	
+	public static int solution(int[] absolutes, boolean[] signs) {
+        int sum = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            sum += signs[i] ? absolutes[i] : -absolutes[i];
+        }
+        return sum;
+    }
+	
+}

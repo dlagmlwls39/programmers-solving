a51e9cb5f28ea23eb9ae28f13b21bf78acd3cec2
dlagm <dlagm@DESKTOP-HK82DOS>
2023-02-11 00:07:36 +0900

    나머지가 1이 되는 수 찾기

---
+++ programmers/src/level1/RemainderOfOne.java
@@ -0,0 +1,26 @@
+// 나머지가 1이 되는 수 찾기(https://school.programmers.co.kr/learn/courses/30/lessons/87389/solution_groups?language=java)
+
+package level1;
+
+import java.util.*;
+
+public class RemainderOfOne {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		System.out.println(solution(n));
+	}
+	
+	public static int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+	
+}

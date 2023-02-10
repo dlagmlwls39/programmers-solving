16de50eebb7d648f4193e65950c02035c91b2acb
Heejin Im <85548480+dlagmlwls39@users.noreply.github.com>
2023-02-11 01:32:36 +0900

    나머지가 1이 되는 수 찾기

--- programmers/src/level1/RemainderOfOne.java
+++ programmers/src/level1/RemainderOfOne.java
@@ -13,14 +13,14 @@ public class RemainderOfOne {
 	}
 	
 	public static int solution(int n) {
-        int answer = 0;
-        for (int i = 2; i <= n; i++) {
-            if (n % i == 1) {
-                answer = i;
-                break;
-            }
-        }
-        return answer;
-    }
+        	int answer = 0;
+        	for (int i = 2; i <= n; i++) {
+            		if (n % i == 1) {
+                		answer = i;
+                		break;
+            		}
+        	}
+        	return answer;
+    	}
 	
 }

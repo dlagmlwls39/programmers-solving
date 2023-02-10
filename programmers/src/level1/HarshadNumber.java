4173a84dd41e4c1c65f182d8f8746f2d35d5cbce
dlagm <dlagm@DESKTOP-HK82DOS>
2023-02-11 00:14:55 +0900

    하샤드 수

---
+++ programmers/src/level1/HarshadNumber.java
@@ -0,0 +1,25 @@
+// 하샤드 수(https://school.programmers.co.kr/learn/courses/30/lessons/12947)
+
+package level1;
+
+import java.util.*;
+
+public class HarshadNumber {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		System.out.println(solution(n));
+	}
+	
+	public static boolean solution(int x) {
+        int sum = 0;
+        int i = x;
+        while (i > 0) {
+            sum += i % 10;
+            i /= 10;
+        }
+        return x % sum == 0;
+    }
+	
+}

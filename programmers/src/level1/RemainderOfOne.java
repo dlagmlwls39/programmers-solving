b847349f003f86958f4613c9529d0b37bff69315
dlagm <dlagm@DESKTOP-HK82DOS>
2023-02-11 00:13:46 +0900

    나머지가 1이 되는 수 찾기

--- programmers/src/level1/RemainderOfOne.java
+++ programmers/src/level1/RemainderOfOne.java
@@ -1,4 +1,4 @@
-// 나머지가 1이 되는 수 찾기(https://school.programmers.co.kr/learn/courses/30/lessons/87389/solution_groups?language=java)
+// 나머지가 1이 되는 수 찾기(https://school.programmers.co.kr/learn/courses/30/lessons/87389)
 
 package level1;
 

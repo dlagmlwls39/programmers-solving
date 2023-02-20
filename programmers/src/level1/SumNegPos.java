7b169525aa4b174333833b5e8ed9e27dbdd5f3b5
Heejin Im <85548480+dlagmlwls39@users.noreply.github.com>
2023-02-21 00:21:27 +0900

    음양 더하기
    음양 더하기

--- programmers/src/level1/SumNegPos.java
+++ programmers/src/level1/SumNegPos.java
@@ -1,4 +1,4 @@
-// 음양 더하기(https://school.programmers.co.kr/learn/courses/30/lessons/76501)
+// 음양 더하기(https://school.programmers.co.kr/learn/courses/30/lessons/76501
 
 package level1;
 
@@ -11,7 +11,7 @@ public class SumNegPos {
 	}
 	
 	public static int solution(int[] absolutes, boolean[] signs) {
-        	int sum = 0;
+		int sum = 0;
         	for (int i = 0; i < absolutes.length; i++) {
             		sum += signs[i] ? absolutes[i] : -absolutes[i];
         	}

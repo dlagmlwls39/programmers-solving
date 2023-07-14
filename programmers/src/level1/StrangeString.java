da16d06cd57f997f054fb6193f1bbd9d1d14a24d
Heejin Im <85548480+dlagmlwls39@users.noreply.github.com>
2023-07-14 15:49:04 +0900

    이상한 문자 만들기

--- programmers/src/level1/StrangeString.java
+++ programmers/src/level1/StrangeString.java
@@ -12,23 +12,23 @@ public class strangeString {
 		System.out.println(solution(s));
 	}
 	
-  public String solution(String s) {
-      StringBuilder answer = new StringBuilder();
-      String [] str = s.split("");
-      int index = 0;  // 단어 기준 인덱스값
-
-      for (String data : str) {
-          // data가 공백이면 index 0으로 초기화, 공백이 아니면 index 증가
-          if (data.equals(" ")) {
-              index = 0;
-              answer.append(data);
-          } else { 
-              // index가 짝수면 대문자, 홀수면 소문자로 변환 후 추가
-              answer.append((index % 2 == 0) ? data.toUpperCase() : data.toLowerCase());
-              index++;
-          }
-      }
-      return answer.toString();
-  }
+	public String solution(String s) {
+		StringBuilder answer = new StringBuilder();
+	      	String [] str = s.split("");
+	      	int index = 0;  // 단어 기준 인덱스값
+	
+	      	for (String data : str) {
+	        	// data가 공백이면 index 0으로 초기화, 공백이 아니면 index 증가
+	          	if (data.equals(" ")) {
+	              		index = 0;
+	              		answer.append(data);
+	          	} else { 
+	              		// index가 짝수면 대문자, 홀수면 소문자로 변환 후 추가
+	              		answer.append((index % 2 == 0) ? data.toUpperCase() : data.toLowerCase());
+	              		index++;
+	          	}
+	      	}
+		return answer.toString();
+	}
 
 }

57028ff75521fbd4ca07e19343d51bcd439fbea1
Heejin Im <dlagmlwls39@naver.com>
2024-01-15 10:50:30 +0900

    소수 만들기

--- programmers/src/level1/MakePrimeNums.java
+++ programmers/src/level1/MakePrimeNums.java
@@ -11,21 +11,22 @@ public class MakePrimeNums {
 	}
 	
 	public String solution(int[] nums) {
-    int answer = 0;
-    int sum = 0;
+	    int answer = 0;
+	    int sum = 0;
     
-    for (int i = 0; i < nums.length-2; i++) {  // 첫 번째 수
-      for (int j = i+1; j < nums.length-1; j++) {  // 두 번째 수
-        for (int k = j+1; k < nums.length; k++) {  // 세 번째 수
-          boolean flag = true;
-          sum = nums[i] + nums[j] + nums[k];
-          for (int h = 2; h * h <= sum; h++) { 
-            if (sum % h == 0) flag = false;  // 소수인지 체크
-          }   
-          if (flag == true) answer++;
-        }
-      }
-    }
-    
-    return answer;
+	    for (int i = 0; i < nums.length-2; i++) {  // 첫 번째 수
+		for (int j = i+1; j < nums.length-1; j++) {  // 두 번째 수
+	        	for (int k = j+1; k < nums.length; k++) {  // 세 번째 수
+	          		boolean flag = true;
+	          		sum = nums[i] + nums[j] + nums[k];
+	          		for (int h = 2; h * h <= sum; h++) { 
+	            			if (sum % h == 0) flag = false;  // 소수인지 체크
+	          		}   
+	          		if (flag == true) answer++;
+			}
+		}
+	    }
+	    
+	    return answer;
+	}
 }
